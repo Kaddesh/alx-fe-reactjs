353242eb8bc68949e3df765e332c3fd6cbe76f4c
Oludamola <oludamolaijimade@gmail.com>
2025-03-01 00:43:26 +0100

    add display style

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: 'blue', color: 'white' }}>
+    <nav style={{ display: "flex", justifyContent: 'center', padding: '10px', backgroundColor: 'blue', color: 'white' }}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/services">Services</Link>
