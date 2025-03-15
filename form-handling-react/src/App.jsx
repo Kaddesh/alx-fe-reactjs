679f46d360bb66403bcdc8b8b413b4b9dc18446d
Oludamola <oludamolaijimade@gmail.com>
2025-03-15 11:22:03 +0100

    formikForm.js

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,12 +1,13 @@
-import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm";
+
 
 function App() {
   return (
     <div className="max-w-md mx-auto mt-10">
       <h2 className="text-xl font-bold mb-4">Controlled Components Form</h2>
       <RegistrationForm />
-      
+
       <h2 className="text-xl font-bold mt-10 mb-4">Formik Form</h2>
       <FormikForm />
     </div>
